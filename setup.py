a4ad3326eb415d6245c36ec9b2706f02085d74df
Shackste <shackst3@gmail.com>
2023-05-10 23:45:13 +0200

    add empty __init__.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='autocut_podcast',
     version='0.1',
     description='Python package for auto-cutting and combining podcast episodes',
+    author='Stefan Hackstein',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
