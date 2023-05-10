9b4d3d5eb0404c140c1478be12ced3722cf22b47
Stefan Hackstein <shackst3@gmail.com>
2023-05-10 23:39:22 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='autocut_podcast',
+    version='0.1',
+    description='Python package for auto-cutting and combining podcast episodes',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'cut = autocut_podcast.cut:main',
+            'combine = autocut_podcast.combine:main',
+            'episodes = autocut_podcast.episodes:main'
+        ]
+    }
+)
