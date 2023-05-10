69c1691889449df0f07ed0a58ab3ad5ac2a8dbdc
Shackste <shackst3@gmail.com>
2023-05-10 23:52:48 +0200

    remove console entry points

--- setup.py
+++ setup.py
@@ -6,11 +6,4 @@ setup(
     description='Python package for auto-cutting and combining podcast episodes',
     author='Stefan Hackstein',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'cut = autocut_podcast.cut:main',
-            'combine = autocut_podcast.combine:main',
-            'episodes = autocut_podcast.episodes:main'
-        ]
-    }
 )
